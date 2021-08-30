e000aa0d5eb51c9475c3dbfc5742664a9394fa5f
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-08-30 17:03:45 -0500

    server & htmlRoutes added, apiRoutes in progress

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,18 @@
 const router = require('express').Router();
 const path = require('path');
 
+// index.html GET routes
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// notes.html GET routes
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 module.exports = router;
