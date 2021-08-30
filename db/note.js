e38f2fcc8594bb68bcb45247dc77f55d15b95d18
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-08-30 17:42:51 -0500

    uuid installed, notes for note.js added

--- db/note.js
+++ db/note.js
@@ -1,8 +1,10 @@
-const util = require('util');
 const fs = require('fs');
 // generate unique id
 const { v4: uuidv4 } = require('uuid');
 
+const util = require('util');
+const readFileSync = util.promisify(fs.readFile);
+const writeFileSync = util.promisify(fs.writeFile);
 // read - readfilesync
 // write - writefilesync
 // get
