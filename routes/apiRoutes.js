fdb2792bdc302716b54b1c5e7adcfa888a3ccb1e
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-08-30 17:14:26 -0500

    notes added for apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
 
+// GET Routes
+router.get('/notes', (req, res) => {})
+
+// POST Routes
+router.post('/notes', (req, res) => {})
+
+// DELETE Routes
+router.delete('/notes/:id', (req, res) => {})
+
 module.exports = router;
