d46265b39a77316bce94b6f131aecf34b5f0ca1c
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-09-03 18:19:43 -0500

    get, post working

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // use apiRoutes
-app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
